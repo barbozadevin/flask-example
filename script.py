47237a4f9cc33acf1a12d8109c9aadea7a648443
barbozadevin <48147753+barbozadevin@users.noreply.github.com>
2021-10-12 19:08:03 +0530

    Update and rename script.py to script.sh

--- script.py
+++
@@ -1,5 +0,0 @@
-pip3 install --upgrade pip
-pip3 install --user virtualenv
-venv flask-env
-.\flask-env\Scripts\activate
-pip3 install -r requirements.txt
---
+++ script.sh
@@ -0,0 +1,5 @@
+pip install --upgrade pip
+pip install --user virtualenv
+venv flask-env
+.\flask-env\Scripts\activate
+pip install -r requirements.txt
