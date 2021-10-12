3499e0f1cd1389402406e261390edb837766ee98
barbozadevin <48147753+barbozadevin@users.noreply.github.com>
2021-10-12 16:33:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-python -m pip install --upgrade pip
-python -m pip install --user virtualenv
-python -m venv flask-env
+python3 -m pip install --upgrade pip
+python3 -m pip install --user virtualenv
+python3 -m venv flask-env
 .\flask-env\Scripts\activate
 pip install -r requirements.txt
