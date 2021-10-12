65203e86e58ed077574166250200e53a5cc14dc4
barbozadevin <48147753+barbozadevin@users.noreply.github.com>
2021-10-12 19:16:16 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-pip install --upgrade pip
-pip install --user virtualenv
-venv flask-env
-.\flask-env\Scripts\activate
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install --user virtualenv
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
